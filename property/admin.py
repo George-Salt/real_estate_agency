1942b6ee94b6dee986a35b01f6b695fb79149203
George <solanikg3@gmail.com>
2023-10-07 13:05:06 +0300

    Connect owners and flats

--- property/admin.py
+++ property/admin.py
@@ -3,6 +3,11 @@ from django.contrib import admin
 from .models import Flat, Complaint, Owner
 
 
+class OwnerInline(admin.TabularInline):
+    model = Flat.owners.through
+    raw_id_fields = ('owner', 'flat')
+
+
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ['address', 'town', 'owner']
     readonly_fields = ['created_at']
@@ -10,13 +15,20 @@ class FlatAdmin(admin.ModelAdmin):
     list_editable = ['new_building']
     list_filter = ['new_building', 'has_balcony', 'active']
     raw_id_fields = ('liked_by',)
+    inlines = [
+        OwnerInline,
+    ]
 
 
 class ComplaintAdmin(admin.ModelAdmin):
     raw_id_fields = ('flat',)
 
+
 class OwnerAdmin(admin.ModelAdmin):
     raw_id_fields = ('flats',)
+    inlines = [
+        OwnerInline,
+    ]
 
 
 admin.site.register(Flat, FlatAdmin)
